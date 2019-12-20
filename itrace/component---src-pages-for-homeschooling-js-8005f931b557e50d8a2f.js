0fe8db6daa36f6d52e58cd1dc594bfa62465ebda
Tatiana Cherednichenko <t.cherednichenko@redmadrobot.com>
2019-12-20 16:14:02 +0300

    add text loading

---
+++ itrace/component---src-pages-for-homeschooling-js-8005f931b557e50d8a2f.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[3],{rum6:function(e,n,a){"use strict";a.r(n);var t=a("q1tI"),m=a.n(t),r=a("cJNW"),c=a("mrHZ");n.default=function(){return m.a.createElement(m.a.Fragment,null,m.a.createElement(c.k,null),m.a.createElement(r.a,{name:"home"},m.a.createElement(c.c,{name:"home"}),m.a.createElement(c.h,{name:"home",noTitle:!0})))}}}]);
